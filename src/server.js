8aac61b3191e10c62b66cae579f799ff8782f9a0
DjDarkz <z.tech4future@gmail.com>
2025-04-12 15:45:20 -0700

    INint

--- src/server.js
+++ src/server.js
@@ -177,7 +177,7 @@ io.on('connection', (socket) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 }); 
